3db6475f3b98353df1146a48925d238f33661bc8
Shih-Min Lee <ssmlee04@gmail.com>
2020-01-12 15:10:23 -0500

    chart first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './../index.css';
+
+export class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Chart;
